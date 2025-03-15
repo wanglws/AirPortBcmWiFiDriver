1f99357fbe71b31fe721a8668dd03e4dd080a499
wanglws <62369263+wanglws@users.noreply.github.com>
2025-03-15 21:02:00 +0800

    Add files via upload

---
+++ AirPortBcmWiFiDriver/AirPortBcmWiFiDriver-High Sierra/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  AirPortBcmWiFiDriver-High Sierra
+//
+//  Created by wanglws on 2025/3/15.
+//
+
+import Foundation
+import NetworkExtension
+
+autoreleasepool {
+    NEProvider.startSystemExtensionMode()
+}
+
+dispatchMain()
